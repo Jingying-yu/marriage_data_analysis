d00c3cefdf8d1039c9c8d4d30c579935ebad688b
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-09-19 16:58:39 -0400

    edited useful files and downloaded data using download script

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,29 @@
+#### Preamble ####
+# Purpose: Simulates data for analyzing marriage dataset
+# Author: Sandy Yu
+# Date: 19 September 2024
+# Contact: jingying.yu@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Sketch ideas
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Simulate data ####
+set.seed(21)
+
+start_date <- as.Date("2023-01-01")
+end_date <- as.Date("2023-12-31")
+
+number_of_dates <- 100
+
+data <-
+  tibble(
+    date = as.Date(runif(n = number_of_dates, min = as.numeric())),
+    number_of_marriage = rpois(n = number_of_dates, lambda = 15)
+  )
+
+
+
