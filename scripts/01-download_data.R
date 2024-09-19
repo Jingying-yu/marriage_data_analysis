6c293a7f6ea26c68db9139ecaed0c9a5a86a821d
Sandy Yu <jingying.yu@mail.utoronto.ca>
2024-09-19 17:09:45 -0400

    cleaned data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from OpenDataToronto
+# Author: Sandy Yu
+# Date: 19 September 2024 
+# Contact: jingying.yu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: planned data with simulate data script
 
 
 #### Workspace setup ####
